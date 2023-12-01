797bed8869f49e481feadb12b91c801102f3750b
agneta1991 <pupienytea@gmail.com>
2023-12-01 11:58:36 +0200

    cors.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Greeting.create(content: 'Hello & Welcome!')
+Greeting.create(content: 'Good to see you! Welcome -from Rails-React app!')
+Greeting.create(content: 'Good afternoon!')
+Greeting.create(content: 'Welcome, its nice to have you here!')
+Greeting.create(content: 'Hello there!')
